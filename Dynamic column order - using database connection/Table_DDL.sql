61ec026e699ed6051c9771337c3d23db14f62687
Aakarsh Rajagopalan <51793634+rajagoah@users.noreply.github.com>
2020-07-24 10:25:22 -0400

    code commit

--- Dynamic column order - using database connection/Table_DDL.sql
+++ Dynamic column order - using database connection/Table_DDL.sql
@@ -7,3 +7,18 @@ create table aakarsh_db.target_table
 , program_start_date date
 );
 commit;
+
+insert into aakarsh_db.target_table
+(therapeutic_area, rfp_id, age)
+values
+('IMM', '101', '21');
+
+insert into aakarsh_db.target_table
+(therapeutic_area, rfp_id, age, load_date, program_start_date)
+values
+('CVM', '101', '19', '2020-07-21','1999-01-01');
+
+insert into aakarsh_db.target_table
+(therapeutic_area, rfp_id, age, load_date, program_start_date)
+values
+('ID', '103', '19','2020-06-22', '2000-01-01');
